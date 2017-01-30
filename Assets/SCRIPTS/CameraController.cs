a8e04eab7406f719f2bdb6be9eb18cf6e6a75513
Dave Schmitz <schmitdm4798@uwec.edu>
2017-01-29 18:14:01 -0600

    Experimented with Movement mechanics

---
+++ Assets/SCRIPTS/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Rigidbody rigidbody = GetComponent<Rigidbody>();
+	    camera.transform.position = new Vector3(rigidbody.position.x,rigidbody.position.y, -10.0f);
+	}
+}
---
+++ Assets/SCRIPTS/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+	    Rigidbody rigidbody = GetComponent<Rigidbody>();
+	    float velocity = rigidbody.velocity.y;
+
+	    float horizontal = Input.GetAxis("Horizontal");
+
+	    if (Input.GetButtonDown("Jump") && velocity <= 0.0001 && velocity >= -0.0001)
+	    {
+	        rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed); 
+	    }
+        
+        Vector2 offset = new Vector2(horizontal, 0.0f) * speed * Time.deltaTime;
+        transform.Translate(offset);
+
+
+	}
+}
