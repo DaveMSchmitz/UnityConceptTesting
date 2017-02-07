6bccfd24ecd449586faec7a738fe864d47515835
Dave Schmitz <schmitdm4798@uwec.edu>
2017-02-06 21:26:53 -0600

    added respawning and a moving background

--- Assets/SCRIPTS/CameraControllerFoo.cs
+++ Assets/SCRIPTS/CameraControllerFoo.cs
@@ -8,24 +8,28 @@ public class CameraControllerFoo : MonoBehaviour
     public GameObject focus;
     public float cameraBuffer;
     public float smoothness;
+    public float clampYpos;
 
     private Vector3 focusPosition;
+    private PlayerControllerFoo playerControllerFoo;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+		playerControllerFoo = focus.GetComponent<PlayerControllerFoo>();
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
 
 	    float x = focus.transform.position.x + Mathf.Sign(focus.transform.localScale.x) * cameraBuffer;
-	    float y = transform.position.y;
+	    float y = Mathf.Clamp(focus.transform.position.y, clampYpos, float.MaxValue);
 	    float z = transform.position.z;
 
         focusPosition = new Vector3(x,y,z);
 	    transform.position = Vector3.Lerp(transform.position, focusPosition, smoothness * Time.deltaTime);
+
 
 	}
 }
--- Assets/SCRIPTS/PlayerControllerFoo.cs
+++ Assets/SCRIPTS/PlayerControllerFoo.cs
@@ -10,12 +10,14 @@ public class PlayerControllerFoo : MonoBehaviour
     public Transform groundSensor;
     public float radius;
     public LayerMask consideredGround;
+    public Vector3 respawnTransform;
 
     private Rigidbody2D playerRigidbody;
     private bool initJump;
     private float horizontal;
     private bool onGround;
     private Animator _playerAnimator;
+    private bool canMove;
 
 
     // Use this for initialization
@@ -23,44 +25,44 @@ public class PlayerControllerFoo : MonoBehaviour
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //check to see if the player is pressing the arrow keys
-        horizontal = Input.GetAxisRaw("Horizontal");
 
-        //check to see if the character is on the ground
-        CheckGround();
-
-        //set the players orientation 
-        if (horizontal !=0)
+        if (canMove)
         {
-            transform.localScale = new Vector3(horizontal, transform.localScale.y);
+            //check to see if the player is pressing the arrow keys
+            horizontal = Input.GetAxisRaw("Horizontal");
+
+            //check to see if the character is on the ground
+            CheckGround();
+
+            //set the players orientation 
+            if (horizontal != 0)
+            {
+                transform.localScale = new Vector3(horizontal, transform.localScale.y);
+            }
+
+            //move the player in the direction that the arrow key was pressed, if it wasn't then horizontal will be zero making the player stop
+            playerRigidbody.velocity = new Vector2(horizontal * movementSpeed * Time.fixedDeltaTime, playerRigidbody.velocity.y);
+
+            //checks if initJump is true. This code is actually already checked but because fixed update is called more than update it is possible to want to
+            //jump and it wont jump so its a second check.
+            if ((Input.GetButtonDown("Jump")) && onGround)
+            {
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed * Time.fixedDeltaTime);
+
+
+            }
+
+
         }
-
-        //move the player in the direction that the arrow key was pressed, if it wasn't then horizontal will be zero making the player stop
-        playerRigidbody.velocity = new Vector2(horizontal * movementSpeed * Time.fixedDeltaTime, playerRigidbody.velocity.y);
-
-        //checks if initJump is true. This code is actually already checked but because fixed update is called more than update it is possible to want to
-        //jump and it wont jump so its a second check.
-        if ((Input.GetButtonDown("Jump")) && onGround)
-        {
-            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed * Time.fixedDeltaTime);
-
-            
-        }
-
 
     }
 
-    void FixedUpdate()
-    {
-        //check if touching ground this frame
-
-
-    }
 
     void LateUpdate()
     {
@@ -73,4 +75,24 @@ public class PlayerControllerFoo : MonoBehaviour
         //check if the ground sensor is touching the ground
         onGround = Physics2D.OverlapCircle(groundSensor.position, radius, consideredGround);
     }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Respawn")
+        {
+            transform.position = respawnTransform;
+
+        }
+    }
+
+    public bool GetCanMove()
+    {
+        return canMove;
+    }
+
+    public void SetCanMove(bool value)
+    {
+        canMove = value;
+    }
+
 }
