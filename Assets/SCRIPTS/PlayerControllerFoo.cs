cd0f0cc23c75788b988867e1b7baae8a4e4d6fd4
Dave Schmitz <schmitdm4798@uwec.edu>
2017-02-27 16:36:16 -0600

    Move Movement controls to MovementController from PlayerController so that MovementController can be put on enemies

--- Assets/SCRIPTS/PlayerControllerFoo.cs
+++ Assets/SCRIPTS/PlayerControllerFoo.cs
@@ -4,85 +4,38 @@ using UnityEngine.Assertions.Comparers;
 
 public class PlayerControllerFoo : MonoBehaviour {
 
-    public float MovementSpeed;
-    public float JumpSpeed;
-    public Transform GroundSensor;
-    public float Radius;
-    public LayerMask ConsideredGround;
+   
     public Vector3 RespawnTransform;
-    public float SlowingDown;
-
-    private Rigidbody2D _playerRigidbody;
-    private float _horizontal;
-    private float _movement;
-    private bool _onGround;
+ 
     private Animator _playerAnimator;
+    private Rigidbody2D _playerRigidbody;
+
+    private MovementController movement;
 
 
-    private bool _triggerJump;
-    private bool _firstTimeStopped;
 
     // Use this for initialization
     void Start() {
-        _playerRigidbody = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        movement = GetComponent<MovementController>();
     }
 
     // Update is called once per frame
     void Update() {
 
-        //get the direction the player wants to move
-        _horizontal = Input.GetAxisRaw("Horizontal");
-
-        //because direction is based on what way the player is moving, don't set orientation if player is not moving
-        if (_horizontal != 0) {
-            //set the players orientation 
-            transform.localScale = new Vector3(Mathf.Sign(_horizontal), transform.localScale.y);
-            _movement = _horizontal;
-        } else {
-            if (Mathf.Abs(_movement) < 0.1)
-            {
-                _movement = 0;
-            }
-            _movement = Mathf.Lerp(_movement, 0, SlowingDown*Time.deltaTime);
-                
-        }
-
-        //check to see if the player is on the ground
-        CheckGround();
-
-        //if the player presses the jump button and is on the ground for this frame set the trigger to true
-        //so that when the next fixedUpdate comes around, jump
-        if (Input.GetButtonDown("Jump") && _onGround) {
-            _triggerJump = true;
-        }
+        
     }
 
-    void FixedUpdate() {
 
-        //if the jump is triggered
-        if (_triggerJump) {
-
-            //add velocity to the player so that they will jump, and reset the trigger
-            _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, JumpSpeed * Time.fixedDeltaTime);
-            _triggerJump = false;
-        }
-
-        //move the player bassed on what the _horizontal was
-        _playerRigidbody.velocity = new Vector2(_movement * MovementSpeed * Time.fixedDeltaTime, _playerRigidbody.velocity.y);
-
-    }
 
     void LateUpdate() {
         //after all of the physics, set the animation of the player
-        _playerAnimator.SetBool("onGround", _onGround);
+        _playerAnimator.SetBool("onGround", movement.CheckGround());
         _playerAnimator.SetFloat("Speed", Mathf.Abs(_playerRigidbody.velocity.x));
     }
 
-    private void CheckGround() {
-        //check if the ground sensor is touching the ground
-        _onGround = Physics2D.OverlapCircle(GroundSensor.position, Radius, ConsideredGround);
-    }
+
 
     void OnTriggerEnter2D(Collider2D obj) {
 
