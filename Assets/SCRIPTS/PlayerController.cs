6b13ed581a96fcb1ec388b2e18dd219a5c05bcc5
Dave Schmitz <schmitdm4798@uwec.edu>
2017-02-28 11:52:13 -0600

    Rename PlayerControllerFoo to PlayerController

--- Assets/SCRIPTS/PlayerController.cs
+++ Assets/SCRIPTS/PlayerController.cs
@@ -1,190 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Security.Principal;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour
 {
-    
-    public float jumpSpeed;
-    public float wallJumpPower;
-    public float maxSpeed;
-    public float groundAcceleration;
-    public float airAcceleration;
-    public float deceleration;
-    public float rayLength;
-    public float sideRayLength;
 
-    public float x;
-    public float y;
+    public Vector3 RespawnTransform;
  
-    private Rigidbody2D playerRigidbody;
-   
-    private float horizontal;
-    private bool initJump = false;
-    private float acceleration;
-    
+    private Animator _playerAnimator;
+    private Rigidbody2D _playerRigidbody;
 
-	// Use this for initialization
-	void Start ()
-	{
-        //set the default rigidbody
-	    playerRigidbody = GetComponent<Rigidbody2D>();
-        //because the player drops in make it easy and set it to air accel
-	    acceleration = airAcceleration;
-
-	}
-	
-	// Update is called once per frame, button presses should be captured in this
-	void Update ()
-	{
-        //capture the button press and set a variable to trigger the physics system to move left or right on next fixed update
-        horizontal = Input.GetAxisRaw("Horizontal");
-       
-
-
-        //capture the button press and set a variable to trigger the physics system to jump on next fixed update
-        if (Input.GetButtonDown("Jump"))
-	    {
-	        initJump = true;
-	    }
-
-        //
-        Debug.DrawRay(transform.position, new Vector2(x, -y).normalized * sideRayLength, Color.blue);
-        Debug.DrawRay(transform.position, new Vector2(-x, -y).normalized * sideRayLength, Color.blue);
-        Debug.DrawRay(transform.position, Vector2.down * rayLength, Color.cyan);
-    }
-
-    void FixedUpdate()
-    {
-       
-        //if the jump has been triggered jump, if not make sure that the input wasnt missed.  Ususally you do not want inputs in the class
-        if ((initJump))
-        {
-            
-            if (CheckGround())
-            {
-                Jump();
-            }
-            else if (CheckWall())
-                WallJump();
-        }
-    
-        Move();
-        initJump = false;
-
-    }
-
-    private void Jump()
-    {
-       playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed * Time.fixedDeltaTime * 10);
-        initJump = false;
-        acceleration = airAcceleration;
+    private MovementController movement;
 
 
 
-    }
-
-    private void WallJump()
-    {
-
-        playerRigidbody.velocity = new Vector2(wallJumpPower * Time.fixedDeltaTime * 13 * -horizontal, wallJumpPower * Time.fixedDeltaTime * 5);
-        initJump = false;
-    }
-
-    private void Move()
-    {
-       
-        float tempDeceleration = 0;
-        if (horizontal == 0 && (int)playerRigidbody.velocity.x != 0)
-        {
-            tempDeceleration = deceleration * -Mathf.Sign(playerRigidbody.velocity.x);
-              
-            
-        }
-
-        float temp = (horizontal * acceleration * Time.fixedDeltaTime * 10) + (playerRigidbody.velocity.x + tempDeceleration);
-        if (horizontal == 1)
-        {
-            temp = Mathf.Clamp(temp, int.MinValue, maxSpeed);
-           
-        }else if (horizontal == -1)
-        {
-           temp = Mathf.Clamp(temp, -maxSpeed, int.MaxValue);
-            
-        }
-
-
-        playerRigidbody.velocity = new Vector2(temp, playerRigidbody.velocity.y);
-    }
-
-
-    private bool CheckGround()
-    {
-        bool result = false;
+    // Use this for initialization
+    void Start() {
         
-        
-        if (Physics2D.Raycast(transform.position,Vector2.down, rayLength))
-        {
-            result = true;
-        }else if (Physics2D.Raycast(transform.position, new Vector2(x , -y), sideRayLength))
-        {
-            
-            result = true;
-        }
-        else if (Physics2D.Raycast(transform.position, new Vector2(-x , -y), sideRayLength))
-        {
-            result = true;
-        }
-        
-
-
-        return result;
+        _playerAnimator = GetComponent<Animator>();
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        movement = GetComponent<MovementController>();
     }
 
-    private bool CheckWall()
-    {
-        bool result = false;
-       
+    // Update is called once per frame
+    void Update() {
 
-
-        if (Physics2D.Raycast(transform.position, Vector2.right, rayLength))
-        {
-            result = true;
-        }
-        else if (Physics2D.Raycast(transform.position,Vector2.left, rayLength))
-        {
-            result = true;
-        }
-        else if (Physics2D.Raycast(transform.position, new Vector2(x, -y), sideRayLength))
-        {
-            result = true;
-        }
-        else if (Physics2D.Raycast(transform.position, new Vector2(-x, -y), sideRayLength))
-        {
-            result = true;
-        }
-        else if (Physics2D.Raycast(transform.position, new Vector2(-x, y), sideRayLength))
-        {
-            result = true;
-        }
-        else if (Physics2D.Raycast(transform.position, new Vector2(-x, -y), sideRayLength))
-        {
-            result = true;
-        }
-
-
-
-
-        return result;
+        
     }
 
-    void OnCollisionEnter2D(Collision2D obj)
-    {
-        if (obj.gameObject.CompareTag("Floor"))
-        {
-            acceleration = groundAcceleration;
+
+
+    void LateUpdate() {
+        //after all of the physics, set the animation of the player
+        _playerAnimator.SetBool("onGround", movement.CheckGround());
+        _playerAnimator.SetFloat("Speed", Mathf.Abs(_playerRigidbody.velocity.x));
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D obj) {
+
+        //if the tag is something that is a respawn, set the players position to what ever to is designated as the RespawnTransform
+        if (obj.tag == "Respawn") {
+            transform.position = RespawnTransform;
+
+        }
+
+        //if the object hit is a checkpoint set the respawn transform to the transform of the object and set the checkpoint animation to set
+        if (obj.tag == "Checkpoint") {
+            RespawnTransform = new Vector3(obj.gameObject.transform.position.x, obj.gameObject.transform.position.y, transform.position.z);
+            obj.gameObject.GetComponent<Animator>().SetBool("check", true);
         }
     }
 }
