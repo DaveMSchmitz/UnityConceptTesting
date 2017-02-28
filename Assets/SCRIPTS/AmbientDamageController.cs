54f13b331830bea1b9310eb157883ffaab1b631e
Noomann <BradTheBlu999@gmail.com>
2017-02-27 20:09:53 -0600

    2/27/2017:2009
    Added AmbientDamageController script to test damage, attempting to link
    up with Health script to deplete player health

---
+++ Assets/SCRIPTS/AmbientDamageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientDamageController : MonoBehaviour {
+    public string name;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == name)
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
