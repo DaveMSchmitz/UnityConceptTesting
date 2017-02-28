9a771a18d2b3aa5a5cb87c4e2f06ee782370d42d
Noomann <BradTheBlu999@gmail.com>
2017-02-28 09:56:16 -0600

    2/28/2017:0953
    Finally figured out a part of damage, object is destroyed when I touch
    it. Tweaks are needed because that's not how damage will work, but it's
    a really good start.

--- Assets/SCRIPTS/AmbientDamageController.cs
+++ Assets/SCRIPTS/AmbientDamageController.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AmbientDamageController : MonoBehaviour {
-    public string name;
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("Testing");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
-    void OnCollisionEnter(Collision col)
+    void OnCollision2DEnter(Collision2D col)
     {
-        if (col.gameObject.name == name)
+        Debug.Log("Collision");
+        if (col.gameObject.name.Equals("dt1"))
         {
             Destroy(col.gameObject);
         }
