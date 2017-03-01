f11f9fbfa6f13f276aeed4fe2fd08856b1ccaf8b
Brad Konsela <BradTheBlu999@gmail.com>
2017-02-28 20:34:10 -0600

    Damage Testing
    Trying to get the damage trigger to decrement health, messing around
    with static fields in the HealthControllerStatic.cs

--- Assets/SCRIPTS/DamageController.cs
+++ Assets/SCRIPTS/DamageController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DamageController : MonoBehaviour {
     public bool takeDamage;
+    public string tag;
     // Use this for initialization
     void Start () {
         Debug.Log("Damage testing");
+        var player = GameObject.FindWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -17,15 +19,20 @@ public class DamageController : MonoBehaviour {
     {
         takeDamage = true;
         //This damages the player
-        if(obj.CompareTag("Damage"))
+        //if(obj.CompareTag("Damage"))
+        if(obj.CompareTag(tag))
         {
+            while (takeDamage){
+                
+            }
             Debug.Log("Dealing damage to player");
         }
     }
 
     void OnTriggerExit2D(Collider2D obj)
     {
-        if (obj.CompareTag("Damage"))
+        //if (obj.CompareTag("Damage"))
+        if(obj.CompareTag(tag))
         {
             takeDamage = false;
             Debug.Log("Stop dealing damage to player");
---
+++ Assets/SCRIPTS/HealthControllerStatic.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+public class HealthControllerStatic : MonoBehaviour
+{
+
+    public static int health;
+    public static int maxHealth;
+    public static bool isAlive;
+
+    public HealthControllerStatic(int mHealth)
+    {
+        //set the health to max health
+        health = maxHealth;
+        maxHealth = mHealth;
+
+        //a check to make sure that the health is bigger than zero
+        if (health > 0)
+        {
+            isAlive = true;
+        }
+    }
+
+    public HealthControllerStatic(int mHealth, int h)
+    {
+        //set the health to max health
+        health = h;
+        maxHealth = mHealth;
+
+        //a check to make sure that the health is bigger than zero
+        if (health > 0)
+        {
+            isAlive = true;
+        }
+    }
+
+    public bool setHealth(int h)
+    {
+        //set the health
+        health = h;
+
+        //if the trying to set health past max health, set to max health instead
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        //if the health is less than or equal to zero change the object to dead
+        if (health <= 0)
+        {
+            isAlive = false;
+        }
+
+        return isAlive;
+    }
+
+    //returns the current health
+    public int getCurrentHealth()
+    {
+        return health;
+    }
+
+    //sets a new maxHealth
+    public bool setMaxHealth(int mHealth)
+    {
+        //set the new maxHealth
+        maxHealth = mHealth;
+
+        //if the current health is higher than the new max health, set the new health to maxHealth
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        return isAlive;
+    }
+
+    //returns the max health
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    //changes the health by what ever the change in health is
+    public bool changeHealth(int deltaHealth)
+    {
+        //change the health by delta
+        health += deltaHealth;
+
+        //check if the object is dead
+        if (health <= 0)
+        {
+            isAlive = false;
+        }
+
+        return isAlive;
+    }
+
+    //takes all health and sets to dead
+    public bool kill()
+    {
+        health = 0;
+        isAlive = false;
+
+        return isAlive;
+    }
+
+    //returns weather the object is dead or alive.
+    public bool getIsAlive()
+    {
+        return isAlive;
+    }
+
+
+}
