74b5c9557dcbf1ece851da7f6d61a1b5aab7f2a9
Dave Schmitz <schmitdm4798@uwec.edu>
2017-03-09 18:09:58 -0600

    worked on enemey controller

---
+++ Assets/SCRIPTS/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class EnemyMovement : MonoBehaviour
+{
+
+    private GameObject _focus;
+
+	// Use this for initialization
+	void Start () {
+		_focus = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_focus != null) {
+			transform.position = _focus.transform.position;
+		}
+	}
+
+    void OnTriggerEnter2D(Collider2D collider){
+		if (collider.CompareTag("Player")) {
+			_focus = collider.gameObject;
+		
+		
+		}
+
+    }
+}
