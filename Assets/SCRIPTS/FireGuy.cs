72bf20c811273e10c600deb0bf50ccee41921699
Dave Schmitz <schmitdm4798@uwec.edu>
2017-04-12 22:43:30 -0500

    created fire guy sleep state:

--- Assets/SCRIPTS/FireGuy.cs
+++ Assets/SCRIPTS/FireGuy.cs
@@ -22,20 +22,44 @@ public class FireGuy : MonoBehaviour {
     [SerializeField]
     private float ProjectileLifeTime = 2;
 
+    [SerializeField]
+    private float SleepTime = 10;
+
+    [SerializeField]
+    private int ShotsPerAttack = 10;
+
     private new Rigidbody2D rigidbody;
-    private Vector3 target;
     private bool executingCo = false;
+    private State currentState = State.Attack;
+
+    //for attack
+    private Vector3 target;
     private GameObject _player;
     private int rotator = 0;
     private int numProjectiles;
     private GameObject[] projectiles;
     private Rigidbody2D[] projectileRB;
 
+
+    //sleep
+    private Vector3 sleepPosition;
+
+
+    
+
+    private int i = 0;
+
+    private enum State {
+        Sleep, Attack
+    }
+
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
         target = FloatingWayPoint.position;
         _player = GameObject.FindGameObjectWithTag("Player");
+
+        sleepPosition = transform.position;
 
         numProjectiles = 1;
 
@@ -49,6 +73,7 @@ public class FireGuy : MonoBehaviour {
         for (int i = 0; i < numProjectiles; ++i) {
             GameObject fire = Instantiate(Projectile, transform.position, transform.rotation);
             projectiles[i] = fire;
+            fire.SetActive(false);
             projectileRB[i] = fire.GetComponent<Rigidbody2D>();
         }
 
@@ -58,7 +83,22 @@ public class FireGuy : MonoBehaviour {
         if (!executingCo) {
             executingCo = true;
 
-            StartCoroutine("shootCo");
+            switch (currentState) {
+                case State.Attack:
+                    StartCoroutine("AttackCoroutine");
+                    break;
+
+                case State.Sleep:
+                    StartCoroutine("SleepCoroutine");
+                    break;
+
+                default:
+                    Debug.Log("State Not In Switch Statement");
+                    break;
+
+
+            }
+            
 
         }
 
@@ -67,32 +107,88 @@ public class FireGuy : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        if (Mathf.Abs(target.y - transform.position.y) < .1) {
-            target = new Vector3(Random.Range(FloatingWayPoint.position.x - 2, FloatingWayPoint.position.x + 2), Random.Range(FloatingWayPoint.position.y - 2, FloatingWayPoint.position.y + 2), transform.position.z);
-        } else {
-            rigidbody.MovePosition(transform.position + (target - transform.position).normalized * (MoveSpeed  * Time.deltaTime));
-        }
+        switch (currentState) {
+            case State.Attack:
+                AttackMovement();
+                break;
 
+            case State.Sleep:
+                SleepMovement();
+                break;
+
+            default:
+                Debug.Log("State Not In Switch Statement");
+                break;
+
+
+        }
         
 	}
 
-    public IEnumerator shootCo() {
+    public IEnumerator AttackCoroutine() {
+
 
         GameObject fire = projectiles[rotator];
+        
         Rigidbody2D fireRB = projectileRB[rotator];
         rotator = (rotator + 1) % numProjectiles;
-        
-        
+
+
 
         Vector3 direction = (_player.transform.position - transform.position).normalized * (10 * ProjectileSpeed);
-        
+        fire.transform.position = transform.position;
+        fire.SetActive(true);
         fireRB.velocity = direction;
-        fireRB.position = transform.position;
+        
 
 
 
         yield return new WaitForSeconds(ShootCoolDown);
 
+        ++i;
+
+        if (i > ShotsPerAttack) {
+
+            currentState = State.Sleep;
+            i = 0;
+            
+        }
+
         executingCo = false;
     }
+
+    public void AttackMovement() {
+
+        if (Mathf.Abs(target.y - transform.position.y) < .1) {
+            target = new Vector3(Random.Range(FloatingWayPoint.position.x - 2, FloatingWayPoint.position.x + 2), Random.Range(FloatingWayPoint.position.y - 2, FloatingWayPoint.position.y + 2), transform.position.z);
+            
+        } else {
+            rigidbody.MovePosition(transform.position + (target - transform.position).normalized * (MoveSpeed * Time.deltaTime));
+            
+        }
+
+    }
+
+    public IEnumerator SleepCoroutine() {
+
+        GetComponentInChildren<ParticleSystem>().Stop();
+        GetComponent<SpriteRenderer>().color = new Color(82f/255, 78f/255, 78f/255, 255/255);
+
+        yield return new WaitForSeconds(SleepTime);
+        currentState = State.Attack;
+        executingCo = false;
+
+        GetComponentInChildren<ParticleSystem>().Play();
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+    }
+
+    public void SleepMovement() {
+
+        if ((Mathf.Abs(transform.position.x - sleepPosition.x) > .1) || (Mathf.Abs(transform.position.y - sleepPosition.y) > .1)) {
+            rigidbody.MovePosition(transform.position + (sleepPosition - transform.position).normalized * (MoveSpeed * Time.deltaTime));
+
+        }
+        
+    }
+
 }
