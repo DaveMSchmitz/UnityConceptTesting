beb5e8564d37e42c1847f37849b9df25db472348
Dave Schmitz <schmitdm4798@uwec.edu>
2017-04-27 16:12:25 -0500

    add talking

---
+++ Assets/SCRIPTS/DialogueManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogueActive;
+
+    public string[] dialogueLines;
+    public int currLine;
+
+	void Start () {
+	}
+	
+	void Update () {
+        if (!dialogueActive && Input.GetKeyDown(KeyCode.Tab))
+        {
+            currLine = 0;
+        }
+        else if (dialogueActive && Input.GetKeyDown(KeyCode.Tab))
+        {
+           
+            currLine++;
+        }
+
+        if(currLine >= dialogueLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogueActive = false;
+            currLine = 0;
+        }
+
+        dText.text = dialogueLines[currLine];
+
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogueActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogueActive = true;
+        dBox.SetActive(true);
+    }
+}
