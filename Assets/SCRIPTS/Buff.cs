b676bfba46e8433923a8e77f538436e5af3afa6d
Brad Konsela <konselbn0125@uwec.edu>
2017-05-02 11:26:04 -0500

    Work on buffs
    Create classes for the three buff types

--- Assets/SCRIPTS/Buff.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Buff : MonoBehaviour
-{
-    abstract public void effect();
-    abstract public void duration();
-}
---
+++ Assets/SCRIPTS/_Buff.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class _Buff : MonoBehaviour {
+    private PlayerAttack atk;
+    private HealthController health;
+    private PlayerMovementController movement;
+
+    abstract public void effect();
+    abstract public void duration();
+}
